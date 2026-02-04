2f794a8312399aabd8a6a42ffd6105a2d11f3070
Adarsh uk <adarshuk79@gmail.com>
2026-02-04 09:25:28 +0530

    ai added

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://collectfee.vercel.app/api",
 });
 
 // url:https://collectfee.onrender.com,https://collectfee.vercel.app/api
